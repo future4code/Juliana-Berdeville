7022ee45861c989ce74f351e80f31746cac82361
Juliana <juliana.berdeville@gmail.com>
2020-04-09 16:54:03 -0300

    Adicionando arquivos sobre controlador de despesas.

---
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -0,0 +1,9 @@
+const secaoCadastro = document.getElementById('cadastro')
+const secaoListaCadastro = document.getElementById('lista-cadastro')
+const secaoextrato = document.getElementById('extrato')
+
+/*Solicitações da seção de cadastro*/
+var valor = document.getElementById('input-valor')
+var tipoDeDespesa = document.getElementById('tipo-despesa').value
+var description = document.getElementById('description')
+/*Solicitações da seção de cadastro*/
