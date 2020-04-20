70a70efe62c1db78c40e279c322101a26a7679a8
Juliana <juliana.berdeville@gmail.com>
2020-04-20 17:02:35 -0300

    Adicionando arquivos do projeto.

---
+++ semana6/f4formslabenu/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import Secao1 from './components/Secao1'
+import Secao2 from './components/Secao2'
+import Secao3 from './components/Secao3'
+import Secao4 from './components/Secao4'
+import './App.css';
+import styled from 'styled-components'
+
+// class App extends React.Component 
+// {
+//   render ()
+function App ()
+  {
+    return 
+    (
+      <div>
+          <h1>Bem-vindo(a)!</h1>
+          <button type="button">NEXT</button>
+      </div>
+    );
+
+  }
+
+export default App;
+
---
+++ semana6/f4formslabenu/src/components/Secao2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components'
+
+function Secao2() {
+  return (
+    <div>
+         <h1>Informações do Ensino Superior (E.S.)</h1>
+        <input type="text" placeholder="Curso"></input>
+        <input type="text" placeholder="Unidade de ensino"></input>
+    </div>
+  );
+}
+
+export default Secao2;
