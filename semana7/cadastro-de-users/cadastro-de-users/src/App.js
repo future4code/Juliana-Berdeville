b26fc3a9a7feea09af74e714bb1d20aab1781573
Juliana Berdeville <jberdeville@MacBook-Pro-de-Juliana.local>
2020-04-29 16:56:37 -0300

    Adicionando arquivos ao projeto II.

--- semana7/cadastro-de-users/cadastro-de-users/src/App.js
+++ semana7/cadastro-de-users/cadastro-de-users/src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Cadastro from './components/Cadastro'
-import Lista from './components/Lista'
+import {Cadastro} from './components/Cadastro'
+import {Lista} from './components/Lista'
 
-function App() {
+export class App extends React.Component {
+ /*Criando state pra segurar o valor de qual página tá sendo acessada */
+  state= {
+      page: 'Lista'
+  }
+/*Criando state pra segurar o valor de qual página tá sendo acessada */
+
+/*Função que, de acordo com o resultado do ternário, altera o estado 'currentPage'*/
+  onClickSwitchPages = () => {
+        const pages = this.state.page === 'Cadastro' ? 'Lista ' : 'Cadastro'
+        this.setState({page: pages})
+  }
+/*Função que, de acordo com o resultado do ternário, altera o estado 'currentPage'*/
+  
+render () {
+
   return (
     <div className="App">
-      <Lista/>
-      <Cadastro/>
+      <h1>Bem-vinde</h1>
+      <button onClick = {this.onClickSwitchPages}>
+        {this.state.page === 'Cadastro' ? 'Lista' : 'Cadastro'}
+      </button>
+      {this.state.page === 'Cadastro' ? <Cadastro/> : <Lista/>}
+      
     </div>
   );
+
+}
 }
 
-export default App;
--- semana7/cadastro-de-users/cadastro-de-users/src/components/Cadastro.js
+++ semana7/cadastro-de-users/cadastro-de-users/src/components/Cadastro.js
@@ -2,31 +2,51 @@ import React from 'react';
 import styled from 'styled-components'
 
 const Bananinha = styled.div` /*sempre que for styled é letra maiuscula*/
-border: 1px solid black;
+border: 1px dotted black;
 width: 40vh;
 height:35vh;
 margin-top: 10vh;
-margin-left: 62vh;
+margin-left: 74vh;
 `
 const Bananete = styled.div`
-  margin-top: 2px;
+  margin-top: 8vh;
   
 `
 
+export class Cadastro extends React.Component {
 
-function Cadastro() {
+  /*Criação do estado que segura os valores inseridos nos inputs*/
+  state = {
+      nameContent: " ",
+      emailContent: " "
+  }
+ /*Criação do estado que segura os valores inseridos nos inputs*/
+
+  /*Alteração do estado pro nome*/
+  onChangeName = (event) => {
+      this.setState({nameContent: event.target.value})
+  }
+  /*Alteração do estado pro nome*/
+
+  /*Alteração do estado pro email*/
+  onChangeEmail = (event) => {
+    this.setState({emailContent: event.target.value})
+}
+  /*Alteração do estado pro email*/
+  render () {
   return (
     <div className="Cadastro">
         <Bananinha>
           <Bananete>
             <label>Nome:</label><br></br>
             <input type="text" name="text" id="text"></input><br></br>
-            <label>E-mail:</label><br></br>
-            <input type="text" name="text" id="text"></input>
+            <br></br><label>E-mail:</label><br></br>
+            <input type="text" name="text" id="text"></input><br></br>
+            <br></br><button>SEND</button>
             </Bananete>
         </Bananinha>
       </div>
   );
 }
+}
 
-export default Cadastro;
