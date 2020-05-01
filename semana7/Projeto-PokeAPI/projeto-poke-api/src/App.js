80af383d463cf5e16972fabc6212edc39ed17ebe
JulianaBerdeville <juliana.berdeville@gmail.com>
2020-05-01 16:18:30 -0300

    Projeto finalizado

--- semana7/Projeto-PokeAPI/projeto-poke-api/src/App.js
+++ semana7/Projeto-PokeAPI/projeto-poke-api/src/App.js
@@ -14,58 +14,83 @@ const Divzinha = styled.div`
 `
 
 const Divzete = styled.div`
-  border: 1px solid black;
   display: flex;
   flex-direction:column;
   justify-content:center;
   align-content:center;
   width: 80vh;
+  height: auto;
   margin-top: 20%;
   
 `
+
+const Fotinho = styled.img`
+  width: 20vh;
+  height: 30vh;
+  margin-top: -15vh;
+  margin-left: -1vh;
+`
+
+const Titles = styled.h1 `
+margin-top: 5vh;
+font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+font-size: 20px;
+`
+const Aqueleh1 = styled.h1 `
+margin-top: 18vh;
+font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+font-size: 20px;
+`
+
+
 /*estado -> lista vazia pra segurar os pokemons que serão buscados pela GET*/
  class App extends React.Component {
  
   state = {
         pokemon: [ ],
-        fotinho: " "
+        fotinho: "",
+        altura: "",
+        peso: ""
   }
 /*estado -> lista vazia pra segurar os pokemons que serão buscados pela GET*/
 
-/*componentDidMount -> lista vazia pra segurar os pokemons que serão buscados pela GET*/
+/*componentDidMount -> lista vazia pra segurar os pokemons que serão buscados pela GET - o que importa é o NOME do pokemon*/
   componentDidMount = () => {
       axios
         .get('https://pokeapi.co/api/v2/pokemon/?limit=151')
         .then(response => {
-                console.log("Puxando tudo sobre todos os pokemons do BD")
-                this.setState({pokemon: response.data.results});
+                this.setState({pokemon: response.data.results})
         })
         .catch(err => {console.log(err)})
   }
-/*componentDidMount -> lista vazia pra segurar os pokemons que serão buscados pela GET*/
+/*componentDidMount -> lista vazia pra segurar os pokemons que serão buscados pela GET - o que importa é o NOME do pokemon*/
 
 /*getNaFotinho -> método que faz uma request (GET) pra puxar só a foto (front_default) do pokemon selecionado*/
-getNaFotinho = event => {
+getNasInfos = event => {
     const pokemonSelecionado = event.target.value
       axios 
         .get(`https://pokeapi.co/api/v2/pokemon/${pokemonSelecionado}`)
         .then(response => {
-            console.log("Pegando a foto do pokemon")
-            this.setState({fotinho: response.data.sprite.front_default})
-        .catch(error => {
-            console.log("Ih, deu erro! Veja: " , error)
-        })
+            this.setState({fotinho: response.data.sprites.front_default})
+            this.setState({nome: response.data.name}) 
+            this.setState({altura: response.data.height})
+            this.setState({peso: response.data.weight})
+          
+          })
+          .catch(err => {
+            console.log(err)
         })
 }
 /*getNaFotinho -> método que faz uma request (GET) pra puxar só a foto (front_default) do pokemon selecionado*/
 
+
 render() {
-  const fotinhoDoPokemon = this.state.fotinho ? (<fotinhoDoPokemon src={this.state.fotinho} alt="É um pokemon"/>) : (<div>{" "}</div>)
+  const fotinhoDoPokemon = this.state.fotinho ? (<Fotinho src={this.state.fotinho} alt=" "/>) : (<div>{" "}</div>)
     return (
       <Divzinha>
-        <h1>Olá e seja bem-vinde à sua Pokedéx: </h1>
-        <h1>Vamos começar? Selecione seu Pokemón:</h1>
-        <select id="pokemons">
+        <Aqueleh1>Olá e seja bem-vinde à sua Pokedéx: </Aqueleh1>
+        <Titles>Vamos começar? Selecione seu Pokemón:</Titles>
+        <select id="pokemons" onChange={this.getNasInfos}>
           <optgroup>
           <option value={" "}>Choose your Pokemon</option> 
               {this.state.pokemon.map (pokemon => {
@@ -74,7 +99,10 @@ render() {
           </optgroup>
         </select>
             <Divzete>
-              <strong>Fotinho:</strong><br></br>{fotinhoDoPokemon}
+                <span>{fotinhoDoPokemon}</span>
+                <p><strong>Nome:</strong> {this.state.nome}</p>
+                <p><strong>Altura:</strong> {this.state.altura} metros</p>
+                <p><strong>Peso:</strong> {this.state.peso} kgs</p>
             </Divzete>
       </Divzinha>
     );
