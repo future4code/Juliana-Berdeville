0d70a125643cf09b9b1b988db90d57d63c7c9f04
JulianaBerdeville <juliana.berdeville@gmail.com>
2020-05-15 16:49:18 -0300

    [Semana9-Projeto]Astromatch

--- semana9/Projeto-Astromatch/astromatch/src/App.js
+++ semana9/Projeto-Astromatch/astromatch/src/App.js
@@ -1,29 +1,50 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Profiles} from './components/Profiles'
+import Profiles from './components/Profiles'
+import Button from '@material-ui/core/Button'
 
 const Background = styled.div`
-  background-color: #242223;
+  background-color: white;
   height: 100vh;
- 
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
 `
 
-const LeButton = styled.button`
+const CheckMatches = styled(Button)`
   display: block;
   bottom:0;
   margin-left: 5vh;
+  background-image: linear-gradient(125deg, #c06c84 0%, #f67280 73%);
+  box-shadow: 0px 4px 32px rgba(252, 56, 56, 0.4);
+  border-radius: 100;
+    :hover
+        {   
+          background-image: linear-gradient(125deg, #c70039 0%, #ff5733 73%);
+          color: white;
+          transition: 1s;
+        }
 `
-
-const AppContentHolder = styled.div`
-   display: flex;
-   align-self: center;
-   justify-content: center;
-   `
-
+const ClearMatches = styled(Button)`
+  display: block;
+  bottom:0;
+  margin-left: 5vh;
+  background-image: linear-gradient(125deg, #c06c84 0%, #f67280 73%);
+  box-shadow: 0px 4px 32px rgba(252, 56, 56, 0.4);
+  border-radius: 100;
+    :hover
+        {
+          background-image: linear-gradient(125deg, #c70039 0%, #900c3f 73%);
+          color: white;
+          transition: 1s;
+        }
+`
+const ContainerGeral = styled.div`
+  height: 85vh;
+  width: 700px;
+`
 const Logo = styled.div`
-  background-color: #454143;
-  height: 90vh;
-  width: 35vh;
   margin-top: 5vh;
   display: flex;
   justify-content: center;
@@ -32,25 +53,35 @@ const Logo = styled.div`
 `
 
 const ContentHolder = styled.div`
-  background-color: #454143;
-  height: 90vh;
-  width: 35vh;
+
   margin-top: 5vh;
+  display: flex;
 `
-const LeLogo = styled.h3
-` 
-  color: white;
-  text-align: center;
+const LeLogo = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Permanent Marker', cursive;
+  font-size: 100px;
 `
+
+/*Ainda em desenvolvimento
+onClickCheckMatches = () => {
+}
+
+onClickClearMatches = () => {
+}
+*/
 
 function App() {
   return (
     <Background>
-      <AppContentHolder>
+      <ContainerGeral>
         <Logo><LeLogo>AstroMatch</LeLogo></Logo>
-        <ContentHolder> {/*<Profiles/>*/} </ContentHolder>
-      </AppContentHolder>
-      <LeButton>clear matches</LeButton>
+        <ContentHolder> <Profiles/> </ContentHolder>
+      </ContainerGeral>
+      <CheckMatches>check matches</CheckMatches>
+      <ClearMatches>clear matches</ClearMatches>
     </Background>
   );
 }
--- semana9/Projeto-Astromatch/astromatch/src/components/Profiles.js
+++ semana9/Projeto-Astromatch/astromatch/src/components/Profiles.js
@@ -1,27 +1,105 @@
+import {useState, useEffect} from 'react'
 import React from 'react'
 import styled from 'styled-components'
-import {useState, useEffect} from 'react'
 import axios from 'axios'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
 
+const ProfileCard = styled(Card)`
+        border-radius: 20px;
+        transition: 0.3s;
+        width: 95%;
+        position: relative;
+        max-width: 800;
+        margin-left: auto;
+        overflow: initial;
+        background: white;
+        display: flex;
+        align-items: center;
+        padding: 2px;
+
+`
+const ProfileCardMedia = styled(CardMedia)`
+          flex-shrink: 0;
+          min-width: 50%;
+          max-height: 100%;
+          padding-top: 48%;
+          box-shadow: 4px 4px 20px 1px rgba(252, 56, 56, 0.2);
+          border-radius: 20px;
+          background-image: linear-gradient(125deg, #c06c84 0%, #f67280 73%); 
+          background-color: white;
+          overflow: hidden;
+`
+const ProfileCardContent = styled(CardContent)`
+          text-align: left;
+          padding-left: 0;
+          padding: 2px;  
+`
+const ProfileBio = styled(Typography)`
+           margin-bottom: 2px;
+`
+const Eww = styled(Button)`
+          background-image: linear-gradient(125deg, #c06c84 0%, #f67280 73%);
+          box-shadow: 0px 4px 32px rgba(252, 56, 56, 0.4);
+          border-radius: 100;
+          padding-left: 24;
+          padding-right: 24;
+          display: flex;
+          justify-content: space-between;
+                    :hover
+                        {
+                            color: #cf2a1b;
+                            transition: 0.5s;
+                        }
+`
+const Wow = styled(Button)`
+          background-image: linear-gradient(125deg, #c06c84 0%, #f67280 73%);
+          box-shadow: 0px 4px 32px rgba(252, 56, 56, 0.4);
+          border-radius: 100;
+          padding-left: 24;
+          padding-right: 24;
+          display: flex;
+          justify-content: space-between;
+                    :hover
+                        {
+                            color: #5fad6f;
+                            transition: 0.5s;
+                        }
+            
+`
 function Profiles() {
 
-    const [pplprofiles, Setpplprofiles] = useState([ ])
+  
+    const [pplProfiles, setpplProfiles] = useState([ ])
 
     useEffect(() => {
         axios
-        .get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/person`)
+        .get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/aluno/person`)
         .then(response => {
-            Setpplprofiles(response.data);
+            setpplProfiles(response.data.profile);
+            console.log(response.data)
         })
         .catch(err => {
             console.log(err);
             });
-    }, [ ]);
+    }, [setpplProfiles]);
 
     return (
-      <div>
-          {pplprofiles}
-      </div>
-    );
+        <ProfileCard> {/*<Card>*/}
+                <ProfileCardMedia image={pplProfiles.photo}/> {/*<CardMedia/>*/}
+            <ProfileCardContent> {/*<CardContent>*/}
+                <ProfileBio>name: {pplProfiles.name}</ProfileBio> {/*Typography*/}
+                <ProfileBio>age: {pplProfiles.age}</ProfileBio> {/*Typography*/}
+                <ProfileBio>bio: {pplProfiles.bio}</ProfileBio> {/*Typography*/}
+                <Eww>eww</Eww> {/*Button*/}
+                <Wow>wow</Wow> {/*Button*/}
+            </ProfileCardContent> {/*<CardContent>*/}
+      </ProfileCard> 
+    )
   }
   export default Profiles;
